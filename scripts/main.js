492029f4950e23b647c1bba8fd10cf16b770ca3b
Michael Gutleber <michaeljgut@gmail.com>
2017-08-09 16:58:08 -0400

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$(function(){
+  // let $gameBoard = $('<div id="game-board"></div>');
+  // $gameBoard.appendTo('body');
+  // let $player1DownCard = $('<img src="images/back.png" id="player1-down-card"/>');
+  // $player1DownCard.appendTo('div');
+  // let $player2DownCard = $('<img src="images/back.png" id="player2-down-card"/>');
+  // $player2DownCard.appendTo('div');
+});
