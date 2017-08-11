7987eb115f6df16f9de5a204a34805746072d036
Michael Gutleber <michaeljgut@gmail.com>
2017-08-11 11:04:16 -0400

    Require each player to make a move

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,10 @@ $(function(){
       suit: '',
       value: ''
     },
+    currentPlayer: '1',
+    timesPressed: 0,
+    player1Card: '',
+    player2Card: '',
     deck: [],
     player1Stack: [],
     player2Stack: [],
@@ -29,46 +33,61 @@ $(function(){
       this.player1Stack = this.deck.slice(0,26);
       this.player2Stack = this.deck.slice(26);
     },
+    getWordValue: function(playerCard) {
+      switch(playerCard.value) {
+        case 'J':
+          return 'jack';
+        case 'Q':
+          return 'queen';
+        case 'K':
+          return 'king';
+        case 'A':
+          return 'ace';
+        default:
+          return playerCard.value;
+      }
+    },
     nextTurn: function() {
-      let player1Card = warGame.player1Stack.pop();
-      let player2Card = warGame.player2Stack.pop();
-      switch(player1Card.value) {
-        case 'J':
-          wordValue = 'jack';
-          break;
-        case 'Q':
-          wordValue = 'queen';
-          break;
-        case 'K':
-          wordValue = 'king';
-          break;
-        case 'A':
-          wordValue = 'ace';
-          break;
-        default:
-          wordValue = player1Card.value;
+      debugger;
+      warGame.timesPressed++;
+      let imgName = '';
+      if (3 === warGame.timesPressed) {
+        imgName = 'images/back.png';
+        $('#player1-main-card').attr('src',imgName);
+        $('#player2-main-card').attr('src',imgName);
+        $('#player2-score').text(warGame.player2Stack.length);
+        $('#player1-score').text(warGame.player1Stack.length);
+        $('#player-turn').text(warGame.currentPlayer);
+        warGame.currentPlayer = '1';
+        warGame.timesPressed = 0;
+        return;
       }
-      let imgName = 'images/' + wordValue + '_of_' + player1Card.suit + '.png';
-      $('#player1-main-card').attr('src',imgName);
+      if ('1' === warGame.currentPlayer) {
+        warGame.player1Card = warGame.player1Stack.shift();
+        wordValue = warGame.getWordValue(warGame.player1Card);
+        imgName = 'images/' + wordValue + '_of_' + warGame.player1Card.suit + '.png';
+        $('#player1-main-card').attr('src',imgName);
+      } else {
+        warGame.player2Card = warGame.player2Stack.shift();
+        wordValue = warGame.getWordValue(warGame.player2Card);
+        imgName = 'images/' + wordValue + '_of_' + warGame.player2Card.suit + '.png';
+        $('#player2-main-card').attr('src',imgName);
+      }
       //$('#player1-secondary-card').attr('src',imgName);
-      switch(player2Card.value) {
-        case 'J':
-          wordValue = 'jack';
-          break;
-        case 'Q':
-          wordValue = 'queen';
-          break;
-        case 'K':
-          wordValue = 'king';
-          break;
-        case 'A':
-          wordValue = 'ace';
-          break;
-        default:
-          wordValue = player2Card.value;
+      if ('2' === warGame.currentPlayer){
+        if (warGame.values.indexOf(warGame.player1Card.value) > warGame.values.indexOf(warGame.player2Card.value)) {
+          warGame.player1Stack.push(warGame.player1Card,warGame.player2Card);
+        }
+        else {
+          warGame.player2Stack.push(warGame.player1Card,warGame.player2Card);
+        }
       }
-      imgName = 'images/' + wordValue + '_of_' + player2Card.suit + '.png';
-      $('#player2-main-card').attr('src',imgName);
+      if ('1' === warGame.currentPlayer) {
+        warGame.currentPlayer = '2';
+      } else {
+        warGame.currentPlayer = '1';
+      }
+      $('#player-turn').text(warGame.currentPlayer);
       //$('#player2-secondary-card').attr('src',imgName);
     },
     newGame: () => {
@@ -83,6 +102,9 @@ $(function(){
       this.buildDeck();
       this.shuffleDeck();
       this.splitDeck();
+      $('#player2-score').text(warGame.player2Stack.length);
+      $('#player1-score').text(warGame.player1Stack.length);
+      $('#player-turn').text(this.currentPlayer);
     }
     // let $gameBoard = $('<div id="game-board"></div>');
     // $gameBoard.appendTo('body');
