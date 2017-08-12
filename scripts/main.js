06c096ea48d5c6b22ee163fa004098cd7c408cc4
Michael Gutleber <michaeljgut@gmail.com>
2017-08-12 14:51:57 -0400

    Basic game is working

--- scripts/main.js
+++ scripts/main.js
@@ -20,9 +20,9 @@ $(function(){
     warWinner: '',
     buildDeck: function() {
       console.log(this.suits);
-        for (var i=0; i < this.suits.length; i++) {
-          for (var j=0; j< this.values.length; j++) {
-          this.deck.push({suit: this.suits[i], value: this.values[j]});
+        for (var i=0; i < this.values.length; i++) {
+          for (var j=0; j< this.suits.length; j++) {
+          this.deck.push({suit: this.suits[j], value: this.values[i]});
           }
         }
     },
@@ -38,6 +38,16 @@ $(function(){
       this.player1Stack = this.deck.slice(0,26);
       this.player2Stack = this.deck.slice(26);
     },
+    splitDeckTest: function() {
+//      while (this.deck.length) {
+        this.player1Stack.push(this.deck.pop());
+        this.player2Stack.push(this.deck.pop());
+//      }
+      this.player1Stack = this.deck.slice(0,25);
+      this.player2Stack = this.deck.slice(25);
+        this.player1Stack.unshift(this.deck.pop());
+        this.player2Stack.unshift(this.deck.pop());
+    },
     getWordValue: function(playerCard) {
       switch(playerCard.value) {
         case 'J':
@@ -52,6 +62,35 @@ $(function(){
           return playerCard.value;
       }
     },
+    getNextCard: function(player) {
+      if ('1' === player) {
+        if (this.player1Stack.length) {
+          return this.player1Stack.shift();
+        } else {
+          if (this.player2Stack.length) {
+            this.warWinner = '2';
+            this.gameState = 'win';
+            return null;
+          } else {
+            this.gameState = 'draw';
+            return null;
+          }
+        }
+      } else {
+        if (this.player2Stack.length) {
+          return this.player2Stack.shift();
+        } else {
+          if (this.player1Stack.length) {
+            this.warWinner = '1';
+            this.gameState = 'win';
+            return null;
+          } else {
+            this.gameState = 'draw';
+            return null;
+          }
+        }
+      }
+    },
     war: function() {
       warGame.gameState = 'war';
       warGame.saveTimesPressed = warGame.timesPressed;
@@ -60,8 +99,19 @@ $(function(){
       warGame.currentPlayer = '1';
       warGame.nextTurn();
     },
+    gameOver: function() {
+      if ('win' === this.gameState) {
+        result = 'Game Over, Player' + this.warWinner + ' Has Won!';
+      } else {
+        result = 'Draw! Game Over!';
+      }
+      $('#result').text(result);
+    },
     nextTurn: function() {
       debugger;
+      if (('win' === this.gameState) || ('draw' === this.gameState)) {
+        alert('Illegal move, game over!');
+      }
       warGame.timesPressed++;
       let imgName = '';
       let rank1 = 0;
@@ -74,7 +124,7 @@ $(function(){
         $('#player2-secondary-card').hide();
       }
       if (3 === warGame.timesPressed) {
-        imgName = 'images/back.png';
+        let imgName = 'images/back.png';
         if ('war' === warGame.gameState) {
           $('#player1-secondary-card').attr('src',imgName);
           $('#player2-secondary-card').attr('src',imgName);
@@ -104,7 +154,11 @@ $(function(){
         return;
       }
       if ('1' === warGame.currentPlayer) {
-        warGame.player1Card = warGame.player1Stack.shift();
+        warGame.player1Card = warGame.getNextCard(this.currentPlayer);
+        if (this.player1Card === null) {
+          this.gameOver();
+          return;
+        }
         wordValue = warGame.getWordValue(warGame.player1Card);
         imgName = 'images/' + wordValue + '_of_' + warGame.player1Card.suit + '.png';
         if ('war' === warGame.gameState) {
@@ -114,7 +168,11 @@ $(function(){
           $('#player1-main-card').attr('src',imgName);
         }
       } else {
-        warGame.player2Card = warGame.player2Stack.shift();
+        warGame.player2Card = warGame.getNextCard(this.currentPlayer);
+        if (this.player2Card === null) {
+          this.gameOver();
+          return;
+        }
         wordValue = warGame.getWordValue(warGame.player2Card);
         imgName = 'images/' + wordValue + '_of_' + warGame.player2Card.suit + '.png';
         if ('war' === warGame.gameState) {
@@ -152,8 +210,17 @@ $(function(){
             $('#player2-secondary-card').attr('src',imgName);
           }
         } else {
-          warGame.player1Card = player1Stack.pop();
-          warGame.player2Card = player2Stack.pop();
+          warGame.warStack.push(warGame.player1Card,warGame.player2Card)
+          warGame.player1Card = this.getNextCard('1');
+          if (this.player1Card === null) {
+            this.gameOver();
+            return;
+          }
+          warGame.player2Card = this.getNextCard('2');
+          if (this.player2Card === null) {
+            this.gameOver();
+            return;
+          }
           warGame.warStack.push(warGame.player1Card,warGame.player2Card)
           warGame.war();
         }
@@ -164,24 +231,45 @@ $(function(){
       else {
         warGame.currentPlayer = '2';
       }
-      $('#player-turn').text(warGame.currentPlayer);
+      let resultText = 'It\'s Player' + this.currentPlayer + '\'s Turn';
+      $('#result').text(resultText);
       //$('#player2-secondary-card').attr('src',imgName);
     },
     newGame: () => {
       console.log('In newGame');
+//      console.log(this);
+      warGame.currentPlayer = '1',
+      warGame.timesPressed = 0;
+      warGame.gameState = '';
+      warGame.player1Stack = [];
+      warGame.player2Stack = [];
+      warGame.deck = [];
+      warGame.buildDeck();
+//      this.shuffleDeck();
+      warGame.splitDeckTest();
+      $('#player2-score').text(warGame.player2Stack.length);
+      $('#player1-score').text(warGame.player1Stack.length);
+      let resultText = 'It\'s Player' + warGame.currentPlayer + '\'s Turn';
+      $('#result').text(resultText);
+      let imgName = 'images/back.png';
+      $('#player1-secondary-card').attr('src',imgName);
+      $('#player2-secondary-card').attr('src',imgName);
+      $('#player1-main-card').attr('src',imgName);
+      $('#player2-main-card').attr('src',imgName);
     },
     start: function() {
       console.log('In init');
       $('#player1-secondary-card').hide();
       $('#player2-secondary-card').hide();
-      $('#next-turn').click(this.nextTurn);
-      $('#new-game').click(this.newGame);
+      $('#next-turn').click(this.nextTurn.bind(warGame));
+      $('#new-game').click(this.newGame.bind(warGame));
       this.buildDeck();
-      this.shuffleDeck();
-      this.splitDeck();
+//      this.shuffleDeck();
+      this.splitDeckTest();
       $('#player2-score').text(warGame.player2Stack.length);
       $('#player1-score').text(warGame.player1Stack.length);
-      $('#player-turn').text(this.currentPlayer);
+      let resultText = 'It\'s Player' + this.currentPlayer + '\'s Turn';
+      $('#result').text(resultText);
     }
     // let $gameBoard = $('<div id="game-board"></div>');
     // $gameBoard.appendTo('body');
